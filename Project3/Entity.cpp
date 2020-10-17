b15ad079bd5bdada82dc96655bc9cca131bb443a
Asma <amk960@nyu.edu>
2020-10-17 14:45:33 -0400

    Initial Commit

---
+++ Project3/Entity.cpp
@@ -0,0 +1,126 @@
+#include "Entity.h"
+
+Entity::Entity()
+{
+    position = glm::vec3(0);
+    movement = glm::vec3(0);
+    acceleration = glm::vec3(0);
+    velocity = glm::vec3(0);
+
+    speed = 0;
+
+    modelMatrix = glm::mat4(1.0f);
+}
+
+void Entity::Update(float deltaTime, Entity *platforms, int platformCount)
+{
+    if (animIndices != NULL) {
+        if (glm::length(movement) != 0) {
+            animTime += deltaTime;
+
+            if (animTime >= 0.25f)
+            {
+                animTime = 0.0f;
+                animIndex++;
+                if (animIndex >= animFrames)
+                {
+                    animIndex = 0;
+                }
+            }
+        }
+        else {
+            animIndex = 0;
+        }
+    }
+    if (jump) {
+        jump = false;
+        velocity.y += jumpPower;
+    }
+    velocity.x = movement.x * speed;
+    velocity += acceleration * deltaTime;
+    position += velocity* deltaTime;
+    
+    for (int i = 0; i < platformCount; i++)
+    {
+        Entity* platform = &platforms[i];
+
+        if (CheckCollision(platform))
+        {
+            float ydist = fabs(position.y - platform->position.y);
+            float penetrationY = fabs(ydist - (height / 2.0f) - (platform->height / 2.0f));
+            if (velocity.y > 0) {
+                position.y -= penetrationY;
+                velocity.y = 0;
+            }
+            else if (velocity.y < 0) {
+                position.y += penetrationY;
+                velocity.y = 0;
+            }
+        }
+    }
+
+
+
+    modelMatrix = glm::mat4(1.0f);
+    modelMatrix = glm::translate(modelMatrix, position);
+}
+
+bool Entity::CheckCollision(Entity* other) {
+    float xdist = fabs(position.x - other->position.x) - ((width + other->width) / 2.0f);
+    float ydist = fabs(position.y - other->position.y) - ((height + other->height) / 2.0f);
+
+    if (xdist < 0 && ydist < 0) return true;
+    return false;
+}
+
+void Entity::DrawSpriteFromTextureAtlas(ShaderProgram* program, GLuint textureID, int index)
+{
+    float u = (float)(index % animCols) / (float)animCols;
+    float v = (float)(index / animCols) / (float)animRows;
+
+    float width = 1.0f / (float)animCols;
+    float height = 1.0f / (float)animRows;
+
+    float texCoords[] = { u, v + height, u + width, v + height, u + width, v,
+        u, v + height, u + width, v, u, v };
+
+    float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program->positionAttribute);
+
+    glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program->texCoordAttribute);
+
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    glDisableVertexAttribArray(program->positionAttribute);
+    glDisableVertexAttribArray(program->texCoordAttribute);
+}
+
+void Entity::Render(ShaderProgram* program) {
+    program->SetModelMatrix(modelMatrix);
+
+    if (animIndices != NULL) {
+        DrawSpriteFromTextureAtlas(program, textureID, animIndices[animIndex]);
+        return;
+    }
+
+    float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+    float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program->positionAttribute);
+
+    glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program->texCoordAttribute);
+
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    glDisableVertexAttribArray(program->positionAttribute);
+    glDisableVertexAttribArray(program->texCoordAttribute);
+}
