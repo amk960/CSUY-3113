db65bb850d42c73a518ce50dd8a88300b8fadbde
Asma <amk960@nyu.edu>
2020-11-22 11:42:04 -0500

    Project 5 initial commit

---
+++ Project5/Level3.cpp
@@ -0,0 +1,108 @@
+#include "Level3.h"
+#define LEVEL3_WIDTH 17
+#define LEVEL3_HEIGHT 8
+
+unsigned int level3_data[] =
+{
+3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+3, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 3,
+3, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+3, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+3, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 3,
+3, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3,
+3, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3
+};
+
+void Level3::Initialize() {
+    //GLuint mapTextureID = Util::LoadTexture("tileset.png");
+    state.nextScene = -1;
+    GLuint mapTextureID = Util::LoadTexture("platformPack_tile001.png");
+    state.map = new Map(LEVEL3_WIDTH, LEVEL3_HEIGHT, level3_data, mapTextureID, 1.0f, 4, 1);
+    // Move over all of the player and enemy code from initialization.
+
+        // Initialize Game Objects
+    // Initialize Player
+    state.player = new Entity();
+    state.player->entityType = PLAYER;
+    state.player->acceleration = glm::vec3(0.0f, -10.0f, 0);
+    state.player->position = glm::vec3(5.0f, 0.0f, 0);
+    state.player->movement = glm::vec3(0);
+    state.player->speed = 2.0f;
+    state.player->textureID = Util::LoadTexture("player.png");
+    state.map = new Map(LEVEL3_WIDTH, LEVEL3_HEIGHT, level3_data, mapTextureID, 1.0f, 4, 1);
+    state.player->width = 0.75f;
+    state.player->height = 0.9;
+    state.player->jumpPower = 5.0f;
+    state.enemy_count = 5;
+
+    //state.platforms = new Entity[PLATFORM_COUNT];
+    state.enemies = new Entity[state.enemy_count];
+    GLuint enemyTextureID = Util::LoadTexture("ctg1.png");
+    GLuint doorTextureID = Util::LoadTexture("playerShip2_green.png");
+    state.door = new Entity();
+    state.door->textureID = doorTextureID;
+    state.door->position = glm::vec3(13.0f, -4.0f, 0);
+    state.door->entityType = PLATFORM;
+    state.door->movement = glm::vec3(0.0f, 0.0f, 0);;
+    state.door->acceleration = glm::vec3(0.0f, 0.0f, 0);;
+    state.door->velocity = glm::vec3(0.0f, 0.0f, 0);
+
+    for (int i = 0; i < state.enemy_count; i++) {
+        state.enemies[i].entityType = ENEMY;
+        state.enemies[i].textureID = enemyTextureID;
+        state.enemies[i].aiType = WAITANDGO;
+        state.enemies[i].aiState = IDLE;
+        state.enemies[i].height = 0.5f;
+        state.enemies[i].width = 0.5f;
+
+        //bottom enemy
+        if (i == 0) {
+            state.enemies[i].position = glm::vec3(1.0f, -3.1f, 0);
+        }
+
+        //middle tier enemy
+        if (i == 1) {
+            state.enemies[i].position = glm::vec3(3.0f, -6.1f, 0);
+        }
+        if (i == 2) {
+            state.enemies[i].position = glm::vec3(10.0f, -4.1f, 0);
+        }
+
+        if (i == 3) {
+            state.enemies[i].position = glm::vec3(7.5f, -1.1f, 0);
+
+        }
+        if (i == 4) {
+            state.enemies[i].position = glm::vec3(8.0f, -5.1f, 0);
+        }
+
+
+        state.enemies[i].speed = 1.0f;
+        state.enemies[i].isActive = true;
+    }
+
+
+}
+void Level3::Update(float deltaTime) {
+    state.player->Update(deltaTime, state.player, state.map, state.enemies, state.enemy_count);
+    state.door->Update(deltaTime, state.player, state.map, state.enemies, state.enemy_count);
+    for (int i = 0; i < state.enemy_count; i++) {
+        if (state.enemies[i].isActive) {
+            state.enemies[i].Update(deltaTime, state.player, state.map, state.enemies, state.enemy_count);
+        }
+    }
+    if (state.player->CheckCollision(state.door) && state.enemy_count == 0) {
+        state.nextScene = 0;
+    }
+}
+void Level3::Render(ShaderProgram* program) {
+    state.map->Render(program);
+    state.door->Render(program);
+    state.player->Render(program);
+    for (int i = 0; i < state.enemy_count; i++) {
+        state.enemies[i].Render(program);
+        //std::cout << "enemy "<<i<< " "<< state.enemies[i].position.x << " "<< state.enemies[i].position.y<< std::endl;
+    }
+    
+}
---
+++ Project5/Menu.cpp
@@ -0,0 +1,39 @@
+#pragma once
+#include "Menu.h"
+#define MENU_WIDTH 14
+#define MENU_HEIGHT 8
+
+unsigned int menu_data[] =
+{
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+};
+GLuint fontTextID;
+void Menu::Initialize() {
+    //GLuint mapTextureID = Util::LoadTexture("tileset.png");
+    state.nextScene = -1;
+    GLuint mapTextureID = Util::LoadTexture("platformPack_tile001.png");
+    fontTextID = Util::LoadTexture("font1.png");
+    state.map = new Map(MENU_WIDTH, MENU_HEIGHT, menu_data, mapTextureID, 1.0f, 4, 1);
+    state.player = new Entity();
+    state.player->entityType = PLAYER;
+
+
+}
+void Menu::Update(float deltaTime) {
+}
+void Menu::Render(ShaderProgram* program) {
+    //state.map->Render(program);
+    Util::DrawText(program, fontTextID, "Welcome to Pathfinder! Press Enter to Start", 0.45f, -0.25f,
+        glm::vec3(1.0f, -1.0f, 0));
+    Util::DrawText(program, fontTextID, "Kill all enemies.", 0.45f, -0.25f,
+        glm::vec3(1.0f, -2.0f, 0));
+    Util::DrawText(program, fontTextID, "Find the Door.", 0.45f, -0.25f,
+        glm::vec3(1.0f, -3.0f, 0));
+}
